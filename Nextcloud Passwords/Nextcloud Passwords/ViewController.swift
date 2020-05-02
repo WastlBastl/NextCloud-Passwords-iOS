bf8b9337b943e833919a849f7216d4734ce9eb65
WastlBastl <s.pollex96@web.de>
2020-05-02 17:06:07 +0200

    Initial Commit

---
+++ Nextcloud Passwords/Nextcloud Passwords/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Nextcloud Passwords
+//
+//  Created by Sebastian Pollex on 02.05.20.
+//  Copyright © 2020 Sebastian Pollex. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
