00ef92ec6c3ca4749ea2071418527f55b4e75a1b
WastlBastl <s.pollex96@web.de>
2020-05-03 20:38:29 +0200

    delete test changes

--- Nextcloud Passwords/Nextcloud Passwords/Main screen/ViewController.swift
+++ Nextcloud Passwords/Nextcloud Passwords/Main screen/ViewController.swift
@@ -15,14 +15,14 @@ class Main: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-//    @IBAction func LoginButton(_ sender: Any) {
+    @IBAction func LoginButton(_ sender: Any) {
         
-//        let storyboard = UIStoryboard(name: "Login", bundle: nil)
+        let storyboard = UIStoryboard(name: "Login", bundle: nil)
         
-//        let vc = storyboard.instantiateViewController(withIdentifier: "LoginStoryboard") as UIViewController
-//        present(vc, animated: true, completion: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "LoginStoryboard") as UIViewController
+        present(vc, animated: true, completion: nil)
         
-//    }
+    }
     
 }
 
