dc512204640f36acd466d7f568833cc3f953e4aa
WastlBastl <s.pollex96@web.de>
2020-05-03 20:07:59 +0200

    Folder restructure, add new class for login

---
+++ Nextcloud Passwords/Nextcloud Passwords/Login/login.swift
@@ -0,0 +1,13 @@
+//
+//  login.swift
+//  Nextcloud Passwords
+//
+//  Created by Sebastian Pollex on 03.05.20.
+//  Copyright © 2020 Sebastian Pollex. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Login: UIViewController {
+}
